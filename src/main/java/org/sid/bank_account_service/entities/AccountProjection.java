edbd65d130e69d0d34990c83e7100caea111236b
YannickFaical <ycompaore48@gmail.com>
2025-02-05 17:54:30 +0100

    ok

--- src/main/java/org/sid/bank_account_service/entities/AccountProjection.java
+++ src/main/java/org/sid/bank_account_service/entities/AccountProjection.java
@@ -3,10 +3,11 @@ package org.sid.bank_account_service.entities;
 import org.sid.bank_account_service.enums.AccountType;
 import org.springframework.data.rest.core.config.Projection;
 
-@Projection(types = BankAccount.class)
+@Projection(types = BankAccount.class ,name = "p1")
 public interface AccountProjection {
     public  String getId();
     public AccountType getType();
+    public Double getBalance();
 
 
 }
--- src/main/java/org/sid/bank_account_service/repositories/BankAccountRepository.java
+++ src/main/java/org/sid/bank_account_service/repositories/BankAccountRepository.java
@@ -4,6 +4,8 @@ import org.sid.bank_account_service.entities.BankAccount;
 import org.sid.bank_account_service.enums.AccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -11,5 +13,6 @@ import java.util.List;
 @RepositoryRestResource
 public interface BankAccountRepository extends JpaRepository<BankAccount,String> {
 
-    List<BankAccount> findByType(AccountType type);
+    @RestResource(path="byType")
+    List<BankAccount> findByType(@PathVariable("y") AccountType type);
 }
